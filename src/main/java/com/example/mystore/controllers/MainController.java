c65cfefb4a6fc205d07e3f83c91184107b2f54e2
Igor Kolpakov <kolpakov.igor@list.ru>
2023-04-12 21:22:39 +0300

    Реализованы функции поиска, сортировки и фильтрации товара по наименованию,цене, категории в личном кабинете зарегистрированного пользователя.

--- src/main/java/com/example/mystore/controllers/MainController.java
+++ src/main/java/com/example/mystore/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.example.mystore.controllers;
 
 import com.example.mystore.models.Person;
+import com.example.mystore.repositories.ProductRepository;
 import com.example.mystore.security.PersonDetails;
 import com.example.mystore.services.PersonService;
 import com.example.mystore.services.ProductService;
@@ -11,10 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
@@ -23,11 +21,13 @@ public class MainController {
     private final PersonService personService;
 
     private final ProductService productService;
+    private final ProductRepository productRepository;
 
-    public MainController(PersonValidator personValidator, PersonService personService, ProductService productService) {
+    public MainController(PersonValidator personValidator, PersonService personService, ProductService productService, ProductRepository productRepository) {
         this.personValidator = personValidator;
         this.personService = personService;
         this.productService = productService;
+        this.productRepository = productRepository;
     }
 
     @GetMapping("/personalAccount")
@@ -62,5 +62,51 @@ public class MainController {
     public String infoProduct(@PathVariable("id") int id, Model model){
         model.addAttribute("product", productService.getProductId(id));
         return "/user/infoProduct";
+    }
+
+    @PostMapping("/personalAccount/product/search")
+    public String productSearch(@RequestParam("search") String search, @RequestParam("ot") String ot, @RequestParam("do") String Do, @RequestParam(value = "price", required = false, defaultValue = "") String price, @RequestParam(value = "contract", required = false, defaultValue = "")String contract, Model model){
+        model.addAttribute("products", productService.getAllProduct());
+
+        if(!ot.isEmpty() & !Do.isEmpty()){
+            if(!price.isEmpty()){
+                if(price.equals("sorted_by_ascending_price")) {
+                    if (!contract.isEmpty()) {
+                        if (contract.equals("furniture")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 1));
+                        } else if (contract.equals("appliances")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 3));
+                        } else if (contract.equals("clothes")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 2));
+                        }
+                    } else {
+                        model.addAttribute("search_product", productRepository.findByTitleOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
+                    }
+                } else if(price.equals("sorted_by_descending_price")){
+                    if(!contract.isEmpty()){
+                        System.out.println(contract);
+                        if(contract.equals("furniture")){
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 1));
+                        }else if (contract.equals("appliances")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 3));
+                        } else if (contract.equals("clothes")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 2));
+                        }
+                    }  else {
+                        model.addAttribute("search_product", productRepository.findByTitleOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
+                    }
+                }
+            } else {
+                model.addAttribute("search_product", productRepository.findByTitleAndPriceGreaterThanEqualAndPriceLessThanEqual(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
+            }
+        } else {
+            model.addAttribute("search_product", productRepository.findByTitleContainingIgnoreCase(search));
+        }
+
+        model.addAttribute("value_search", search);
+        model.addAttribute("value_price_ot", ot);
+        model.addAttribute("value_price_do", Do);
+        return "/product/product";
+
     }
 }
