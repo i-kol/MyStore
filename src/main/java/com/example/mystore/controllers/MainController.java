a7c69beff633da4dc44c4591373e7919741dfefa
Igor Kolpakov <kolpakov.igor@list.ru>
2023-04-12 03:31:46 +0300

    Реализована функция просмотра товара незарегистрированными пользователями и обычными пользователями с ролью user.

--- src/main/java/com/example/mystore/controllers/MainController.java
+++ src/main/java/com/example/mystore/controllers/MainController.java
@@ -3,14 +3,17 @@ package com.example.mystore.controllers;
 import com.example.mystore.models.Person;
 import com.example.mystore.security.PersonDetails;
 import com.example.mystore.services.PersonService;
+import com.example.mystore.services.ProductService;
 import com.example.mystore.util.PersonValidator;
 import jakarta.validation.Valid;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -19,13 +22,16 @@ public class MainController {
     private final PersonValidator personValidator;
     private final PersonService personService;
 
-    public MainController(PersonValidator personValidator, PersonService personService) {
+    private final ProductService productService;
+
+    public MainController(PersonValidator personValidator, PersonService personService, ProductService productService) {
         this.personValidator = personValidator;
         this.personService = personService;
+        this.productService = productService;
     }
 
-    @GetMapping("/index")
-    public String index(){
+    @GetMapping("/personalAccount")
+    public String index(Model model){
         // Получаем объект аутентификации -> с помощью SpringContextHolder обращаемся к контексту и на нем вызываем метод аутентификации. Из сессии текущего пользователя получаем объект, который был положен в данную сессию после аутентификации пользователя
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
@@ -33,7 +39,8 @@ public class MainController {
         if(role.equals("ROLE_ADMIN")){
             return "redirect:/admin";
         }
-        return "index";
+        model.addAttribute("products", productService.getAllProduct());
+        return "/user/index";
     }
 
     @GetMapping("/registration")
@@ -48,6 +55,12 @@ public class MainController {
             return "registration";
         }
         personService.register(person);
-        return "redirect:/index";
+        return "redirect:/personalAccount";
+    }
+
+    @GetMapping("/personalAccount/product/info/{id}")
+    public String infoProduct(@PathVariable("id") int id, Model model){
+        model.addAttribute("product", productService.getProductId(id));
+        return "/user/infoProduct";
     }
 }
